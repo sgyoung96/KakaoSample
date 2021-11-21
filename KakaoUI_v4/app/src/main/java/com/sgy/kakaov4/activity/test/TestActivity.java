facac1b48de62f371fa4afb1e379183ce44955b4
송가영 <sgyoung96@naver.com>
2021-11-21 14:33:50 +0900

    ListView 실습할 TestActivity 생성

---
+++ KakaoUI_v4/app/src/main/java/com/sgy/kakaov4/activity/test/TestActivity.java
@@ -0,0 +1,42 @@
+package com.sgy.kakaov4.activity.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.sgy.kakaov4.activity.BaseActivity;
+
+// ListView 를 사용해서 채팅목록을 만들어보자
+public class TestActivity extends BaseActivity {
+
+    @Override
+    public void createActivity() {
+
+    }
+
+    @Override
+    public void resumeActivity() {
+
+    }
+
+    @Override
+    public void pauseActivity() {
+
+    }
+
+    @Override
+    public void destroyActivity() {
+
+    }
+
+    @Override
+    public void onViewClick(View v) {
+
+    }
+
+    @Override
+    public void setStatusbarPadding(int height) {
+
+    }
+}
